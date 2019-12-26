978a7885ad77581cf88ee327e6e12f85203db9cf
lenakud <lenakudl31@gmail.com>
2019-12-26 16:09:50 +0300

    new section

--- js/main.js
+++ js/main.js
@@ -13,5 +13,16 @@ jQuery(document).ready(function () {
         },
       });
 
+     
+ $(document).ready(function(){
+    $(".navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+
 });
 
